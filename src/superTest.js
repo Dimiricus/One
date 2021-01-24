ff3b6ac226e4fa1e4862766740eed63bb4d39b7f
Dimiricus <getdimiricus@gmail.com>
2021-01-25 01:19:00 +0300

    Init

---
+++ src/superTest.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const SuperTest = () => {
+    return (
+        <div className="superTest">
+            <p>This is super test!</p>
+        </div>
+    );
+}
+
+export default SuperTest;
